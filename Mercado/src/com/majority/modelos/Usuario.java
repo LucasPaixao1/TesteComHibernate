c64db6da2625c990b399da31a13304899856a1a4
lucas <lucas.paixao20@etec.sp.gov.br>
2019-06-19 22:28:23 -0300

    Projeto criado contendo a classe de modelo do usuario

---
+++ Mercado/src/com/majority/modelos/Usuario.java
@@ -0,0 +1,42 @@
+package com.majority.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
+	private String Login;
+	private String Senha;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getLogin() {
+		return Login;
+	}
+	
+	public void setLogin(String login) {
+		Login = login;
+	}
+	
+	public String getSenha() {
+		return Senha;
+	}
+	
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+	
+}
